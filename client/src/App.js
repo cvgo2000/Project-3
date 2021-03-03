b82ca96194c8b500d9c6bb38ac0df531beddc059
Devyn Bailey <devynbailey11@yahoo.com>
2021-03-02 17:23:58 -0700

    cleaned up directory, added bootstrap and react packages, added navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+    <>
+    <Navbar />
+    </>
+    )
 }
 
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from 'react-scroll';
+//Font Awesome Imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-success fixed-top">
+      <div className="container">
+        <div className="app-name">
+            Tour De Hops
+        </div>
+        
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link
+                smooth={true}
+                to="home"
+                offset={-50}
+                className="nav-link"
+                href="#"
+              >
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                smooth={true}
+                to="about"
+                offset={-50}
+                className="nav-link"
+                href="#"
+              >
+                Search
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                smooth={true}
+                to="portfolio"
+                offset={-50}
+                className="nav-link"
+                href="#"
+              >
+                Favorites
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                smooth={true}
+                to="contact"
+                offset={-50}
+                className="nav-link"
+                href="#"
+              >
+                Log In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
