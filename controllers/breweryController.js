01377eb6d63b3b2bcffd493dbe0cea15fec21077
Cody Chen <codychen1@gmail.com>
2021-03-13 11:00:58 -0700

    added findRandom function to search random trail in db

--- controllers/breweryController.js
+++ controllers/breweryController.js
@@ -12,18 +12,29 @@ module.exports = {
       .catch((err) => console.log(err));
   },
 
+  //find a random brewery to showcase on front page
+  findRandom: function (req, res) {
+    var rand = Math.floor(Math.random() * 5);
+    db.Brewery.findOne()
+      .skip(rand)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+
   //find a brewery by ID (Probably won't use this one)
   findById: function (req, res) {
     db.Brewery.findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+
   //add a new brewery to DB
   create: function (req, res) {
     db.Brewery.create(req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+
   //find a brewery by city (FindAll accomplishes this)
   // findByCity: function (req, res) {
   //   console.log(req.body);
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+//exports trails and brewery js files to controllers
+
 module.exports = {
   Trail: require("./trailModels/trail.js"),
   Brewery: require("./breweryModels/brewery.js"),
