e980c4051d4e74b08bd02fcf4f1cd11027d55c4e
Devyn Bailey <devynbailey11@yahoo.com>
2021-03-14 12:36:47 -0600

    edited seedDB, editing styling/images to results

--- client/src/components/SearchResults/SearchBrewResult.js
+++ client/src/components/SearchResults/SearchBrewResult.js
@@ -1,29 +1,34 @@
 import React from "react";
-import { Card, CardDeck } from "react-bootstrap";
+import { Card, CardDeck, Nav, Button } from "react-bootstrap";
 import "../Cards/style.css";
+import coBrewImg from "../../img/co-craft-beer.jpg";
 
 const SearchBrewResult = ({ breweries }) => {
-
   return (
     <div className="container">
       <div className="results">
-        
         Brewery Results
         {breweries.map((brewery) => (
           <CardDeck>
             <Card key={brewery.id} style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="https://static.wixstatic.com/media/6f16e4_75c18fcd4018499eacda48cda995e385~mv2.png/v1/fill/w_560,h_480,al_c,q_85,usm_0.66_1.00_0.01/6f16e4_75c18fcd4018499eacda48cda995e385~mv2.webp" />
+              <Card.Img variant="top" src={coBrewImg} />
               <Card.Body>
                 <Card.Title>{brewery.name}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">
                   {brewery.city}
                 </Card.Subtitle>
                 <Card.Text>
-                  {brewery.street}
-                  {brewery.phone}
+                  Address: {brewery.street}
+                  <br />
+                  Phone: {brewery.phone}
                 </Card.Text>
-                <Card.Link href="#">{brewery.website_url}</Card.Link>
               </Card.Body>
+              <Card.Footer className="text-muted">
+                <Nav.Item>
+                  <Nav.Link href={brewery.website_url} target="_blank">Visit Site</Nav.Link>
+                </Nav.Item>
+                <Button variant="primary">Save Brewery</Button>
+              </Card.Footer>
             </Card>
           </CardDeck>
         ))}
--- client/src/components/SearchResults/SearchTrailResult.js
+++ client/src/components/SearchResults/SearchTrailResult.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardDeck } from "react-bootstrap";
+import { Card, CardDeck, Nav, Button } from "react-bootstrap";
 import "../Cards/style.css";
 
 const SearchTrailResult = ({ filteredTrails }) => {
@@ -11,18 +11,24 @@ const SearchTrailResult = ({ filteredTrails }) => {
         {filteredTrails.map((trail) => (
           <CardDeck>
             <Card key={trail.id} style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="https://cdn2.apstatic.com/photos/hike/7053480_large_1555703487.jpg" />
+            <Card.Img variant="top" src={trail.image} />
               <Card.Body>
                 <Card.Title>{trail.trail}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">
                   {trail.location}
                 </Card.Subtitle>
                 <Card.Text>
-                  {trail.distance}
-                  {trail.difficulty}
+                  Distance: {trail.distance}
+                  <br />
+                  Difficulty: {trail.difficulty}
                 </Card.Text>
-                <Card.Link href="#">{trail.link}</Card.Link>
               </Card.Body>
+              <Card.Footer className="text-muted">
+                <Nav.Item>
+                  <Nav.Link href={trail.link} target="_blank">Visit Site</Nav.Link>
+                </Nav.Item>
+                <Button variant="primary">Save Trail</Button>
+              </Card.Footer>
             </Card>
           </CardDeck>
         ))}
