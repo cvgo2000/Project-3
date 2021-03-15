4b24f53071bc44a11a1fb26f2ce04a48cac544f5
Cody Chen <codychen1@gmail.com>
2021-03-14 19:39:01 -0600

    added React components for favorites page

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,30 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { StoreProvider } from "./utils/GlobalState";
 
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import UserSignIn from "./pages/UserSignIn";
 import Search from "./pages/Search";
 import Favorites from "./pages/Favorites";
 import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signin" component={UserSignIn} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/favorites" component={Favorites} />
-        </Wrapper>
-        <Footer />
+        <StoreProvider>
+          <Navbar />
+          <Wrapper>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signin" component={UserSignIn} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/favorites" component={Favorites} />
+          </Wrapper>
+          <Footer />
+        </StoreProvider>
       </div>
     </Router>
   );
